c19e3a4d508727fbe078a2fe03d929fca943fe98
Néo ELDUAYEN <nelduayen@iutbayonne.univ-pau.fr>
2024-12-06 00:23:36 +0100

    Ajout des cards dans le projet...

---
+++ src/app/components/info/info.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-info',
+  imports: [CommonModule, NgFor],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.scss'
+})
+export class InfoComponent {
+  constructor() {}
+
+  // fait moi un tableau info qui contient des objets contenant un titre, une description et une thumbnail
+  infos = [
+    {
+      title: 'Angular',
+      description: 'Angular is a platform and framework for building single-page client applications using HTML and TypeScript.',
+      thumbnail: 'https://angular.io/assets/images/logos/angular/angular.png',
+      datePublication: '2020-01-01'
+    },
+    {
+      title: 'React',
+      description: 'React is a declarative, efficient, and flexible JavaScript library for building user interfaces.',
+      thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+      datePublication: '2020-01-02'
+    },
+    {
+      title: 'Vue',
+      description: 'Vue.js is a progressive framework for building user interfaces.',
+      thumbnail: 'https://vuejs.org/images/logo.png',
+      datePublication: '2020-01-03'
+    }
+  ];
+
+}
