e6c02ba39ee6be8d69bdec71dfd7eb0cce187e2b
neoelduayen <nelduayen@univ-pau.fr>
2024-12-06 00:41:06 +0100

    Ajout d'un message 'Date inconnue' si aucune date n'est trouvée

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -10,7 +10,6 @@ import { CommonModule, NgFor } from '@angular/common';
 export class InfoComponent {
   constructor() {}
 
-  // fait moi un tableau info qui contient des objets contenant un titre, une description et une thumbnail
   infos = [
     {
       title: 'Angular',
@@ -29,6 +28,42 @@ export class InfoComponent {
       description: 'Vue.js is a progressive framework for building user interfaces.',
       thumbnail: 'https://vuejs.org/images/logo.png',
       datePublication: '2020-01-03'
+    },
+    {
+      title: 'Angular',
+      description: 'Angular is a platform and framework for building single-page client applications using HTML and TypeScript.',
+      thumbnail: 'https://angular.io/assets/images/logos/angular/angular.png',
+      datePublication: '2020-01-01'
+    },
+    {
+      title: 'React',
+      description: 'React is a declarative, efficient, and flexible JavaScript library for building user interfaces.',
+      thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+      datePublication: '2020-01-02'
+    },
+    {
+      title: 'Vue',
+      description: 'Vue.js is a progressive framework for building user interfaces.',
+      thumbnail: 'https://vuejs.org/images/logo.png',
+      datePublication: '2020-01-03'
+    },
+    {
+      title: 'Angular',
+      description: 'Angular is a platform and framework for building single-page client applications using HTML and TypeScript.',
+      thumbnail: 'https://angular.io/assets/images/logos/angular/angular.png',
+      datePublication: '2020-01-01'
+    },
+    {
+      title: 'React',
+      description: 'React is a declarative, efficient, and flexible JavaScript library for building user interfaces.',
+      thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+      datePublication: '2020-01-02'
+    },
+    {
+      title: 'Vue',
+      description: 'Vue.js is a progressive framework for building user interfaces.',
+      thumbnail: 'https://vuejs.org/images/logo.png',
+      
     }
   ];
 
