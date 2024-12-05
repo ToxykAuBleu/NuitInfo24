9feae47b4f931c2b06a82a601f1c5f9f5b3b1b3e
Adrien Hiribarren Touya <adrien64250@yahoo.com>
2024-12-05 23:49:39 +0100

    ✨ Init a-propos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from "@angular/router";
+import { AProposComponent } from "./components/a-propos/a-propos.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'a-propos', component: AProposComponent},
+];
