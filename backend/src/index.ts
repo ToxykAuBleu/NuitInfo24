c46d624fe516a8048a63abc7e4857cd0eb6e3e2a
Romain PINSOLLE <romain.pins@gmail.com>
2024-12-06 02:32:37 +0100

    final honey pot + fusion back et front

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import loginRoutes from './routes/loginRoutes';
 import dashboardRoutes from './routes/dashboardRoutes'; // Import dashboardRoutes
+import path from 'path';
 
 const app = express();
 const port = 3000;
@@ -14,6 +15,15 @@ app.use(express.static(__dirname + '/public')); // Serve static files
 
 app.use('/login', loginRoutes);
 app.use('/dashboard', dashboardRoutes); // Use dashboardRoutes
+
+
+// app main page
+app.use(express.static(path.join(__dirname, '../../dist/nuit-info24/browser')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../dist/nuit-info24/browser', 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
