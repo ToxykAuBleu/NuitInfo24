cf061e15ca248ecec55b24ee3f0af05f6728da3d
Lucas E. <espiet.l@valbion.com>
2024-12-06 06:06:58 +0100

    Multiple ajouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { AProposComponent } from "./components/a-propos/a-propos.component";
 import { InfoComponent } from "./components/info/info.component";
 import { JeuComponent } from "./components/jeu/jeu.component";
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: 'info', component: InfoComponent },
-    { path: 'jeu', component: JeuComponent },
-    { path: 'a-propos', component: AProposComponent },
+  { path: '', component: HomeComponent },
+  { path: 'a-propos', component: AProposComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'jeu', component: JeuComponent },
 ];
