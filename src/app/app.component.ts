5ed56ba9eabbe6c44e5ec125e44a730bc5cd190b
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2024-12-06 02:49:55 +0100

    :sparkles: Inclusion du bouton dans le 'app-component'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { LetterShifterComponent } from './components/letter-shifter/letter-shifter.component';
+import { EscapeButtonComponent} from './components/escape-button/escape-button.component';
 
 @Component({
 	selector: "app-root",
-	imports: [RouterOutlet, LetterShifterComponent],
+	imports: [RouterOutlet, LetterShifterComponent, EscapeButtonComponent],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
