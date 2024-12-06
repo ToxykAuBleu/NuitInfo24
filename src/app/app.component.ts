3461e3e5706340f688e6a647124606899299cbe9
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-12-06 04:16:08 +0100

    :sparkles: Affichage d'une popup par minute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { LetterShifterComponent } from './components/letter-shifter/letter-shifter.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @Component({
 	selector: "app-root",
-	imports: [RouterOutlet, LetterShifterComponent],
+	imports: [RouterOutlet, LetterShifterComponent, PopupComponent],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
 	title = "NuitInfo24";
+  popupOpen = false;
+  ngOnInit() {
+    this.startPopup();
+  }
+
+  startPopup() {
+    setInterval(() => {
+      this.popupOpen = true;
+    }, 60000);
+  }
+
+  closePopup() {
+    this.popupOpen = false;
+  }
 }
